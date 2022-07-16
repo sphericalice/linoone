8f09b71614e34fca4356d4e3c4a4c73b4cd1f6e3
SphericalIce <sphericalice@outlook.com>
2022-07-16 20:36:27 +0100

    Update the evolution_methods settings file

--- settings/evolution_methods.py
+++ settings/evolution_methods.py
@@ -5,7 +5,7 @@
 #--------------------------------------------------------------------
 from enum import Enum
 
-EvolutionParamTypes = Enum("EvolutionParamTypes", "none level item gt_number")
+EvolutionParamTypes = Enum("EvolutionParamTypes", "none level item gt_number move type mapsec mon map")
 
 # Evolution methods metadata settings that can't be automatically
 # determined from source files inspection.
@@ -86,6 +86,136 @@ class EvolutionMethodSettings:
                 "id": "EVO_BEAUTY",
                 "description": "Beauty",
                 "param_type": EvolutionParamTypes.gt_number,
+            },
+            "16": {
+                "id": "EVO_LEVEL_FEMALE",
+                "description": "Level up (Female)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "17": {
+                "id": "EVO_LEVEL_MALE",
+                "description": "Level up (Male)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "18": {
+                "id": "EVO_LEVEL_NIGHT",
+                "description": "Level up (Night)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "19": {
+                "id": "EVO_LEVEL_DAY",
+                "description": "Level up (Day)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "20": {
+                "id": "EVO_LEVEL_DUSK",
+                "description": "Level up (Dusk)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "21": {
+                "id": "EVO_ITEM_HOLD_DAY",
+                "description": "Level up (Holding item during the day)",
+                "param_type": EvolutionParamTypes.item,
+            },
+            "22": {
+                "id": "EVO_ITEM_HOLD_NIGHT",
+                "description": "Level up (Holding item during the night)",
+                "param_type": EvolutionParamTypes.item,
+            },
+            "23": {
+                "id": "EVO_MOVE",
+                "description": "Level up (Knowing a specific move)",
+                "param_type": EvolutionParamTypes.move,
+            },
+            "24": {
+                "id": "EVO_MOVE_TYPE",
+                "description": "Level up (Knowing a move of a specific type)",
+                "param_type": EvolutionParamTypes.type,
+            },
+            "25": {
+                "id": "EVO_MAPSEC",
+                "description": "Level up (In a specified map)",
+                "param_type": EvolutionParamTypes.mapsec,
+            },
+            "26": {
+                "id": "EVO_ITEM_MALE",
+                "description": "Use item (Male)",
+                "param_type": EvolutionParamTypes.item,
+            },
+            "27": {
+                "id": "EVO_ITEM_FEMALE",
+                "description": "Use item (Female)",
+                "param_type": EvolutionParamTypes.item,
+            },
+            "28": {
+                "id": "EVO_LEVEL_RAIN",
+                "description": "Level up (While it's raining)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "29": {
+                "id": "EVO_SPECIFIC_MON_IN_PARTY",
+                "description": "Level up (With specified mon in party)",
+                "param_type": EvolutionParamTypes.mon,
+            },
+            "30": {
+                "id": "EVO_LEVEL_DARK_TYPE_MON_IN_PARTY",
+                "description": "Level up (With Dark-type in party)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "31": {
+                "id": "EVO_TRADE_SPECIFIC_MON",
+                "description": "Trade for a specific mon",
+                "param_type": EvolutionParamTypes.mon,
+            },
+            "32": {
+                "id": "EVO_SPECIFIC_MAP",
+                "description": "Level up on a specific map",
+                "param_type": EvolutionParamTypes.map,
+            },
+            "33": {
+                "id": "EVO_LEVEL_NATURE_AMPED",
+                "description": "Level up (Amped nature)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "34": {
+                "id": "EVO_LEVEL_NATURE_LOW_KEY",
+                "description": "Level up (Low Key nature)",
+                "param_type": EvolutionParamTypes.level,
+            },
+            "35": {
+                "id": "EVO_CRITICAL_HITS",
+                "description": "Lands enough critical hits",
+                "param_type": EvolutionParamTypes.gt_number,
+            },
+            "36": {
+                "id": "EVO_SCRIPT_TRIGGER_DMG",
+                "description": "Pokémon has specified HP below max, then player interacts trigger",
+                "param_type": EvolutionParamTypes.none,
+            },
+            "37": {
+                "id": "EVO_DARK_SCROLL",
+                "description": "Interacts with Scroll of Darkness",
+                "param_type": EvolutionParamTypes.none,
+            },
+            "38": {
+                "id": "EVO_WATER_SCROLL",
+                "description": "Interacts with Scroll of Water",
+                "param_type": EvolutionParamTypes.none,
+            },
+            "65533": {
+                "id": "EVO_PRIMAL_REVERSION",
+                "description": "Primal Reversion",
+                "param_type": EvolutionParamTypes.none,
+            },
+            "65534": {
+                "id": "EVO_MOVE_MEGA_EVOLUTION",
+                "description": "Mega Evolution with a move",
+                "param_type": EvolutionParamTypes.move,
+            },
+            "65535": {
+                "id": "EVO_MEGA_EVOLUTION",
+                "description": "Mega Evolution",
+                "param_type": EvolutionParamTypes.item,
             },
         }
 
