01d1eb4cf85f9647d71d07a6706fd2a5030ef42f
sphericalice <sphericalice@outlook.com>
2022-07-18 13:14:44 +0100

    Handle Castform & Cherrim's filepaths

--- generators/mon_pics.py
+++ generators/mon_pics.py
@@ -21,13 +21,25 @@ class MonPicsGenerator(BaseGenerator):
         os.makedirs(poke_images_dir, exist_ok=True)
         self.generate_mon_pics(self.core_data["mon_front_pics"], self.core_data["species_to_national"], "front", (0, 0, 64, 64), self.core_data["mon_palettes"])
         self.generate_mon_pics(self.core_data["mon_back_pics"], self.core_data["species_to_national"], "back", (0, 0, 64, 64), self.core_data["mon_palettes"])
-        self.generate_shiny_mon_pics(self.core_data["mon_front_pics"], self.core_data["species_to_national"], "front", (0, 0, 64, 64), self.core_data["mon_shiny_palettes"])
-        self.generate_shiny_mon_pics(self.core_data["mon_back_pics"], self.core_data["species_to_national"], "back", (0, 0, 64, 64), self.core_data["mon_shiny_palettes"])
+        self.generate_mon_pics(self.core_data["mon_front_pics"], self.core_data["species_to_national"], "front_shiny", (0, 0, 64, 64), self.core_data["mon_shiny_palettes"])
+        self.generate_mon_pics(self.core_data["mon_back_pics"], self.core_data["species_to_national"], "back_shiny", (0, 0, 64, 64), self.core_data["mon_shiny_palettes"])
         self.generate_mon_pics(self.core_data["mon_icon_pics"], self.core_data["species_to_national"], "icon", (0, 0, 32, 32))
 
         type_images_dir = os.path.join(self.config["dist_dir"], "images/types")
         os.makedirs(type_images_dir, exist_ok=True)
         self.generate_type_pics(self.core_data["type_names"], self.project_settings["types"])
+
+
+    def get_correct_filepath(self, filepath, filetype, new_filetype):
+        """
+        Process the mon's gfx filepath while taking into account the special paths for Castform and Cherrim.
+        """
+        result = re.sub(filetype, new_filetype, filepath)
+        if not os.path.exists(result):
+            result = re.sub(r"(.*)/(.*)", r"\1/normal/\2", filepath)
+            result = re.sub(filetype, new_filetype, result)
+        return result
+
 
     def generate_mon_pics(self, species_to_pics, species_to_national, name, crop, mon_palettes=[], force=False):
         """
@@ -37,8 +49,7 @@ class MonPicsGenerator(BaseGenerator):
             if species not in species_to_national:
                 continue
 
-            filepath = species_to_pics[species]
-            png_filepath = re.sub(r"\.4bpp.*", ".png", filepath)
+            png_filepath = self.get_correct_filepath(species_to_pics[species], r"\.4bpp.*", ".png")
             dest_filepath = os.path.join(self.config["dist_dir"], "images/pokemon/%s_%s.png" % (species_to_national[species], name))
             if force or not os.path.exists(dest_filepath):
                 if not os.path.exists(png_filepath):
@@ -47,36 +58,11 @@ class MonPicsGenerator(BaseGenerator):
                     img = Image.open(png_filepath)
                     cropped_img = img.crop(crop)
                     if mon_palettes is not [] and img.mode == "P" and species in mon_palettes and os.path.exists(mon_palettes[species]) and (force or not os.path.exists(dest_filepath)):
-                        palette_filepath = re.sub(r"\.gbapal.*", ".pal", mon_palettes[species])
+                        palette_filepath = self.get_correct_filepath(mon_palettes[species], r"\.gbapal.*", ".pal")
                         palette = parse_jasc_file(palette_filepath)
                         if palette is not None:
                             cropped_img.putpalette(palette)
                     cropped_img.save(dest_filepath, transparency=0, optimize=1)
-
-
-    def generate_shiny_mon_pics(self, species_to_pics, species_to_national, name, crop, mon_shiny_palettes, force=False):
-        """
-        Processes and generates the various shiny mon images into the distribution directory.
-        """
-        for species in species_to_pics:
-            if species not in species_to_national:
-                continue
-
-            filepath = species_to_pics[species]
-            png_filepath = re.sub(r"\.4bpp.*", ".png", filepath)
-            dest_filepath = os.path.join(self.config["dist_dir"], "images/pokemon/%s_%s_shiny.png" % (species_to_national[species], name))
-            if force or not os.path.exists(dest_filepath):
-                try:
-                    img = Image.open(png_filepath)
-                    cropped_img = img.crop(crop)
-                    if img.mode == "P" and species in mon_shiny_palettes and os.path.exists(mon_shiny_palettes[species]) and (force or not os.path.exists(dest_filepath)):
-                        palette_filepath = re.sub(r"\.gbapal.*", ".pal", mon_shiny_palettes[species])
-                        shiny_palette = parse_jasc_file(palette_filepath)
-                        if shiny_palette is not None:
-                            cropped_img.putpalette(shiny_palette)
-                            cropped_img.save(dest_filepath, transparency=0, optimize=1)
-                except FileNotFoundError:
-                    print("Skipping shiny %s pic for species %s because %s doesn't exist." % (name, species, png_filepath))
 
 
     def generate_type_pics(self, type_names, type_settings, force=False):
